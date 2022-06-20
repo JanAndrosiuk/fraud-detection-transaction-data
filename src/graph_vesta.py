ab1d25685f3e953e7620e6309d355d728f210ce1
JanAndrosiuk <26jasiek@gmail.com>
2022-06-21 00:38:56 +0200

    Fixed issue with merging Vesta graph features

--- src/graph_vesta.py
+++ src/graph_vesta.py
@@ -13,41 +13,49 @@ class GraphVesta:
                  y_train_path="../data/processed/ieee_train_y.pkl",
                  neo4j_root_dir="C:/Users/user/.Neo4jDesktop/relate-data/dbmss/" +
                                 "dbms-c3534c51-313d-4ff0-9094-0475b8061d1b/",
-                 raw_train_transaction_path="../data/raw/train_identity.csv",
-                 raw_train_identity_path="../data/raw/train_identity.csv",
+                 raw_imputed_path="../data/interim/ieee_train_imputed_old.pkl",
                  db_uri="bolt://localhost:7687", user="neo4j", password="password",
-                 graph_name="vesta", nodes_labels="['account', 'device']", rel_label="transaction"):
+                 graph_name="vesta", nodes_labels="['account', 'device']", rel_label="transaction",
+                 save_prefix="vesta_"):
         self.x_train_path, self.y_train_path = x_train_path, y_train_path
-        self.raw_train_transaction_path = raw_train_transaction_path,
-        self.raw_train_identity_path = raw_train_identity_path
+        self.raw_imputed_path = raw_imputed_path
         self.neo4j_root_dir = neo4j_root_dir
         self.x_train, self.y_train = None, None
         self.train_nodes, self.train_edges = None, None
         self.signature_cols, self.metrics_cols = ["start_node_id", "end_node_id"], None
         self.gds, self.db_uri, self.user, self.password = None, db_uri, user, password
         self.graph_name, self.nodes_labels, self.rel_label = graph_name, nodes_labels, rel_label
+        self.save_prefix = save_prefix
 
     def load_raw_data(self):
         with open(self.x_train_path, "rb") as handle:
             self.x_train = pickle.load(handle)
         with open(self.y_train_path, "rb") as handle:
             self.y_train = pickle.load(handle)
+        logger.debug(f"Data set shapes -> x_train: {self.x_train.shape}, y_train: {self.y_train.shape}")
 
         return 0
 
     def generate_signatures(self):
-        cols = list(self.x_train.columns)
+
+        with open(self.raw_imputed_path, "rb") as h:
+            train_imp = pickle.load(h)
+
+        cols = list(train_imp.columns)
         start_node_sign_cols = list(filter(re.compile("card").match, cols))
+        logger.debug(f"Columns used to create signature: {start_node_sign_cols}")
         end_node_sign_cols = list(filter(re.compile("DeviceInfo").match, cols))
 
-        start_node_signature = self.x_train.groupby(start_node_sign_cols).ngroup().values
-        end_node_signature = self.x_train.groupby(end_node_sign_cols).ngroup().values
+        start_node_signature = train_imp.groupby(start_node_sign_cols).ngroup().values
+        end_node_signature = train_imp.groupby(end_node_sign_cols).ngroup().values
 
-        self.x_train[self.signature_cols[0]] = start_node_signature
-        self.x_train[self.signature_cols[0]] = "a"+self.x_train[self.signature_cols[0]].astype(str)
+        df_res = pd.DataFrame()
+        df_res[self.signature_cols[0]] = start_node_signature
+        df_res[self.signature_cols[0]] = "a"+df_res[self.signature_cols[0]].astype(str)
+        df_res[self.signature_cols[1]] = end_node_signature
+        df_res[self.signature_cols[1]] = "v"+df_res[self.signature_cols[1]].astype(str)
 
-        self.x_train[self.signature_cols[1]] = end_node_signature
-        self.x_train[self.signature_cols[1]] = "v"+self.x_train[self.signature_cols[1]].astype(str)
+        self.x_train = pd.concat([self.x_train, df_res], axis=1)
 
         return 0
 
@@ -58,35 +66,37 @@ class GraphVesta:
         """
 
         # 1. Account nodes
-        logger.info("Preparing Account nodes")
+        logger.info("Preparing and saving Account nodes")
         df_to_save = pd.DataFrame(self.x_train["start_node_id"].unique())
         df_to_save.columns = ["start_node_id:ID"]
         df_to_save["label:LABEL"] = "account"
         feature_header = list(df_to_save.columns)
         # noinspection PyTypeChecker
         np.savetxt(
-            f"{self.neo4j_root_dir}import/nodes_account_header.csv", (feature_header,), delimiter=",", fmt="% s"
+            f"{self.neo4j_root_dir}import/{self.save_prefix}nodes_account_header.csv",
+            (feature_header,), delimiter=",", fmt="% s"
         )
         df_to_save.to_csv(
-            f"{self.neo4j_root_dir}import/nodes_account.csv", header=False, index=False
+            f"{self.neo4j_root_dir}import/{self.save_prefix}nodes_account.csv", header=False, index=False
         )
 
         # 2. Device nodes
-        logger.info("Preparing Device nodes")
+        logger.info("Preparing and saving Device nodes")
         df_to_save = pd.DataFrame(self.x_train["end_node_id"].unique())
         df_to_save.columns = ["end_node_id:ID"]
         df_to_save["label:LABEL"] = "device"
         feature_header = list(df_to_save.columns)
         # noinspection PyTypeChecker
         np.savetxt(
-            f"{self.neo4j_root_dir}import/nodes_device_header.csv", (feature_header,), delimiter=",", fmt="% s"
+            f"{self.neo4j_root_dir}import/{self.save_prefix}nodes_device_header.csv",
+            (feature_header,), delimiter=",", fmt="% s"
         )
         df_to_save.to_csv(
-            f"{self.neo4j_root_dir}import/nodes_device.csv", header=False, index=False
+            f"{self.neo4j_root_dir}import/{self.save_prefix}nodes_device.csv", header=False, index=False
         )
 
         # 3. Relationship edges
-        logger.info("Preparing Relationship nodes")
+        logger.info("Preparing and saving Relationship nodes")
         dtype_neo4j_dict = {"int64": "int", "float64": "float", "float32": "float", "object": "string"}
         feature_dtypes = self.x_train.dtypes.replace(dtype_neo4j_dict)
         features = feature_dtypes.index
@@ -99,21 +109,24 @@ class GraphVesta:
         df_to_save["type"] = "transaction"
         # noinspection PyTypeChecker
         np.savetxt(
-            f"{self.neo4j_root_dir}import/rels_header.csv", (feature_header,), delimiter=",", fmt="% s"
+            f"{self.neo4j_root_dir}import/{self.save_prefix}rels_header.csv",
+            (feature_header,), delimiter=",", fmt="% s"
         )
         df_to_save.to_csv(
-            f"{self.neo4j_root_dir}import/rels.csv", header=False, index=False
+            f"{self.neo4j_root_dir}import/{self.save_prefix}rels.csv", header=False, index=False
         )
 
         return 0
 
     def create_bat_file(self):
-        with open("../src/neo4j_ieee_import.bat", "w+") as h:
+        with open(f"../src/neo4j_{self.save_prefix}import.bat", "w+") as h:
             h.write(
                 f"cd {self.neo4j_root_dir}\n" + "bin/neo4j-admin.bat import " +
-                "--nodes import/nodes_account_header.csv,import/nodes_account.csv " +
-                "--nodes import/nodes_device_header.csv,import/nodes_device.csv " +
-                "--relationships import/rels_header.csv,import/rels.csv " +
+                f"--nodes import/{self.save_prefix}nodes_account_header.csv," +
+                f"import/{self.save_prefix}nodes_account.csv " +
+                f"--nodes import/{self.save_prefix}nodes_device_header.csv," +
+                f"import/{self.save_prefix}nodes_device.csv " +
+                f"--relationships import/{self.save_prefix}rels_header.csv,import/{self.save_prefix}rels.csv " +
                 "--force"
             )
         return 0
@@ -133,7 +146,7 @@ class GraphVesta:
                 )
                 """
             )
-            print("Graph successfully created")
+            logger.info("Graph successfully created")
         except Exception:
             raise Exception("wrong graph parameters / db server not started")
 
@@ -193,34 +206,32 @@ class GraphVesta:
         # Calculate size of the community each node belongs to
         map_dict = louvain_size.groupby("communityId").size().to_frame()
         map_dict.columns = ["community_size"]
-        louvain_size = louvain_size.merge(map_dict, left_on="communityId", right_index=True)\
+        louvain_size = louvain_size.merge(map_dict, left_on="communityId", right_index=True) \
             .drop(["nodeId", "communityId"], axis=1)
 
         # Collect all metrics in one dataframe describing each node
         metrics_df = pd.concat(
             [degree, pagerank, hits, betweenness, wcc_size, louvain_size], axis=1
         )
+        metrics_acc = metrics_df.loc[metrics_df["id"].str[0] == "a"].add_prefix("acc_")
+        metrics_dev = metrics_df.loc[metrics_df["id"].str[0] == "v"].add_prefix("dev_")
 
         # Merge calculated metrics with train dataset
         self.x_train = self.x_train.merge(
-            metrics_df.add_prefix("start_"), how="inner", left_on="start_node_id", right_on="start_id"
+            metrics_acc, how="left", left_on="start_node_id", right_on="acc_id"
         )
-        self.x_train.drop("start_id", axis=1, inplace=True)
         self.x_train = self.x_train.merge(
-            metrics_df.add_prefix("end_"), how="inner", left_on="end_node_id", right_on="end_id"
+            metrics_dev, how="left", left_on="end_node_id", right_on="dev_id"
         )
-        self.x_train.drop(
-            ["end_id", "start_pagerank", "start_hits_auth", "start_betweenness",
-             "end_degree", "end_hits_hub", "end_betweenness"],
-            axis=1, inplace=True
-        )
+        self.x_train.drop(["start_node_id", "end_node_id", "acc_id", "dev_id"], axis=1, inplace=True)
 
         self.metrics_cols = [x for x in list(self.x_train.columns) if x not in init_cols]
+        logger.debug(f"Metric columns: {self.metrics_cols}")
 
         if save_dataset:
             if not os.path.exists("../data/processed"):
                 os.mkdir("../data/processed")
-            with open(f"../data/processed/ieee_graph_train_0.pkl", "wb") as handle:
+            with open(f"../data/processed/{self.save_prefix}graph_train_0.pkl", "wb") as handle:
                 pickle.dump(self.x_train, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
         return 0
---
+++ src/neo4j_vesta_import.bat
@@ -0,0 +1,2 @@
+cd C:/Users/user/.Neo4jDesktop/relate-data/dbmss/dbms-c3534c51-313d-4ff0-9094-0475b8061d1b/
+bin/neo4j-admin.bat import --nodes import/vesta_nodes_account_header.csv,import/vesta_nodes_account.csv --nodes import/vesta_nodes_device_header.csv,import/vesta_nodes_device.csv --relationships import/vesta_rels_header.csv,import/vesta_rels.csv --force
